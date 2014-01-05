ae2e81cfbb3f6b2837448292e6a562e7d36ebf56
dinimicky <dinimicky@gmail.com>
2014-01-05 23:09:05 +0800

    realize parse_grid function

--- src/shudu.erl
+++ src/shudu.erl
@@ -4,36 +4,103 @@
 
 -module(shudu).
 -compile(export_all).
+
+-define(CROSS(AL,BL),[[A,B]||A<-AL, B<-BL]).
+-define(DIGITS, "123456789").
+-define(ROWS, "ABCDEFGHI").
+-define(COLS, ?DIGITS).
+-define(SQUARES, ?CROSS(?ROWS, ?COLS)).
+-define(UNITLISTS, [?CROSS(?ROWS, [C])||C<-?COLS]++
+					[?CROSS([R], ?COLS)||R<-?ROWS]++
+					[?CROSS(RS, CS)||CS<-["123","456","789"], RS<-["ABC", "DEF", "GHI"]]).
+-define(UNITS, [{S, [Unit||Unit<-?UNITLISTS, lists:member(S, Unit)]}||S<-?SQUARES]).
+-define(PEERS, [{S, lists:umerge(SL)--[S]}||{S,SL}<-?UNITS]).
+-define(GRID, "800000000003600000070090200050007000000045700000100030001000068008500010090000400").
+
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([]).
+-export([parse_grid/1]).
 
-
+display(ValuesDict)->
+	Width = 1+lists:max([length(dict:fetch(S, ValuesDict))||S<-?SQUARES]),
+	Line = string:copies("+"++string:copies("-", 3*Width),3)++"+",
+	Center = fun(Elem) ->
+					 Head = (Width-length(Elem)) div 2,
+					 Tail = Width - length(Elem) - Head,	  
+					 string:chars($ , Head)++Elem++string:chars($ , Tail)	  
+			 end,
+	StringEle = fun(R, C) ->
+						Elem = dict:fetch([R,C], get(values)),
+						case lists:member(C, "36") of
+							true -> Center(Elem)++"|";
+							false -> Center(Elem)
+						end
+				end,
+	PrintRow = fun(R) -> 
+					   Row = lists:flatten([StringEle(R,C)||C<-?COLS]),
+					   case lists:member(R, "CF") of   
+						   true -> Row++"\n"++Line++"\n";   
+						   false -> Row++"\n"   
+					   end
+			   end,
+	ValStr = lists:foldl(fun(R, AccIn) -> Row = PrintRow(R), AccIn++Row end, [], ?ROWS),
+	lists:foldl(fun(R, ok)-> io:format("~p~n", [R]) end, ok, string:tokens(ValStr, "\n")),
+	ValStr.
+parse_grid(Grid)->
+	put(peers, dict:from_list(?PEERS)),
+	put(units, dict:from_list(?UNITS)),
+	Values = [{S, ?DIGITS}||S<-?SQUARES],
+	put(values, dict:from_list(Values)),
+	dict:map(fun(S,D)->case lists:member(D, ?DIGITS) of
+						   true -> assign(S, D);
+						   false -> pass
+					   end
+			 end, grid_values(Grid)),
+	get(values).
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
-exclusion(P, SDList)->
-	{X, Y} = position(P),
-	Col = column(X, SDList),
-	Row = row(Y, SDList),
-	Area = area(X,Y,SDList),
-	lists:seq(1, 9) -- Col -- Row -- Area.
-	
+grid_values(Grid)->
+	Chars = [C||C<-Grid, lists:member(C, ?DIGITS++"0.")],
+	dict:from_list(lists:zip(?SQUARES, Chars)).
 
-column(C, SDList)->
-	[Ele||I<-lists:seq(0, 8), (Ele = lists:nth(I*9+C+1, SDList)) =/=0].
-row(R, SDList)->
-	[Ele||I<-lists:seq(0, 8), (Ele = lists:nth(R*9+I+1, SDList))=/=0].
-area(X,Y, SDList)->
-	X1 = X div 3 * 3,
-	X2 = (X div 3 + 1) * 3,
-	Y1 = Y div 3 * 3,
-	Y2 = (Y div 3 + 1) * 3,
-	[Ele||I<-lists:seq(X1, X2-1), J<-lists:seq(Y1, Y2-1), (Ele = lists:nth(I+J*9+1, SDList)) =/= 0].
+assign(S, D)->
+	Other_values=lists:delete(D, dict:fetch(S, get(values))),
+	[eliminate(S,D2)||D2<-Other_values],
+	get(values).
 
-position(I)->
-	{I rem 9, I div 9}.
+eliminate(S, D)->
+	case lists:member(D, dict:fetch(S, get(values))) of
+		false -> get(values);
+		true ->
+			put(values, dict:update(S, fun(VS) -> lists:delete(D, VS) end, get(values))),
+			case dict:fetch(S, get(values)) of
+				[] -> throw({error, false});
+				[D2] -> [eliminate(S2,D2) ||S2<-dict:fetch(S, get(peers))];
+				_ -> pass
+			end,
+			F1 = fun(U,D) ->
+						 DPlaces = [SS||SS<-U, lists:member(D, dict:fetch(SS, get(values)))],
+						 case DPlaces of 
+							 [] -> throw({error, false});
+							 [NewS] -> assign(NewS, D);
+							 _ -> pass
+						 end,
+						 ok
+				 end,
+			[F1(U,D)||U<-dict:fetch(S, get(units))],
+			get(values)
+	end.
 
+test()->
+	Units = dict:from_list(?UNITS),
+	Peers = dict:from_list(?PEERS),
+	io:format("~p~n~p~n~p~n~p~n~p~n~p~n", [?SQUARES,?UNITLISTS,Units,Peers,dict:fetch("A1", Units), dict:fetch("A1", Peers)]).
+test_grid_values()->
+	Grid = "003020600900305001001806400008102900700000008006708200002609500800203009005010300",
+	grid_values(Grid).
 
+test_parse_grid()->
+	parse_grid(?GRID).
